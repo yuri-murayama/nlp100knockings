db089a0bb980fb020ab0756633abeadef8a70b7f
KanaOzaki <31536464+KanaOzaki@users.noreply.github.com>
2019-05-07 12:42:39 +0900

    delete chap1

--- kana/chap1/00.py
+++
@@ -1,5 +0,0 @@
-#-*- coding: utf-8 -*-
-
-text = "stressed"
-
-print (text[::-1])	
--- kana/chap1/01.py
+++
@@ -1,7 +0,0 @@
-#-*- coding: utf-8 -*-
-
-text = "パタトクカシーー"
-
-for i in range(0, len(text),2):
-	print(text[i], end = "")
-print ()
