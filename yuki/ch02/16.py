d34833b12157bb55a03665ebfa50fdd54f880a3e
田屋侑希 <yuki@tayayuuki-no-MacBook-Air.local>
2019-05-08 14:37:57 +0900

    10-19

--- yuki/ch02/16.py
+++ yuki/ch02/16.py
@@ -1,11 +1,22 @@
+import sys
 with open('hightemp.txt', 'r' ) as f:
-    N = int(input())
+    args = sys.argv
+    lis = []
     l = f.readlines()
+    n = int(args[1])
     for i in range(len(l)):
-        if i % N != 0:
-            print(l[i])
+        if (i+1) % n == 0:
+            lis.append(l[i])
+            with open('split{0:02}.txt'.format(i), 'w') as f2:
+                for j in range(len(lis)):
+                    f2.write(lis[j])
+                lis = []
         else:
-            print('-------------------------')
-
+            lis.append(l[i])
+    if len(l) % n != 0:
+        with open('splitlast.txt', 'w') as f2:
+            for j in range(len(lis)):
+                f2.write(lis[j])
+            
 #split -l 分割数 hightemp.txt
 #これを実行するとファイルが分割されて新しいファイルができている
