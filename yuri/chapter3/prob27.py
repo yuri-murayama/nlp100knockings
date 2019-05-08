2801a63fb77bdce93db47ffb8743f145d5ff851d
村山友理 <Yuri@murayamayuri-no-MacBook-Air.local>
2019-05-08 18:47:33 +0900

    add 27

---
+++ yuri/chapter3/prob27.py
@@ -0,0 +1,18 @@
+from prob21 import fopen
+import re
+
+def to_text(l):
+    l = re.sub(r'\'{2,5}', r'', l)
+    pattern = r'\[\[([^\]]+?\|)*(.*?)\]\]'
+    l = re.sub(pattern, r'\2', l)
+    return l
+
+t = fopen()
+pattern1 = r'基礎情報.*?^(.*?)^\}\}'
+l = re.findall(pattern1, t, flags=re.MULTILINE | re.DOTALL)
+pattern2 = r'^\|(.*?)\s\=\s(.*?)$'
+l = re.findall(pattern2, l[0], flags=re.MULTILINE)
+d = {line[0]: to_text(line[1]) for line in l}
+
+for k, v in d.items():
+    print(k, v)
