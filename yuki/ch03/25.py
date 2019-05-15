cf520bcb4d4ef7394a0f5563e74126101343ba5e
田屋侑希 <yuki@tayayuuki-no-MacBook-Air.local>
2019-05-15 14:57:41 +0900

    20-29

---
+++ yuki/ch03/25.py
@@ -0,0 +1,16 @@
+import re
+with open('jawiki-country-EU.json', 'r') as f:
+    lines = f.read()
+    m1 = re.search(r'(\{\{基礎情報\s国)(.*?)',lines)
+    m2 = re.search(r'(\}\})\n',lines[m1.end():])
+    with open('kisojoho.json', 'w') as f2:
+        f2.write(lines[m1.end()+1:m2.end()-1])
+
+with open('kisojoho.json','r') as f3:
+    lines = f3.read().split('\n')
+    dic = {}
+    for line in lines:
+        m = re.match(r'^(\|)(.*?)\s=\s(.*?)$',line)
+        if m != None:
+            dic.update([(m.group(2),m.group(3))])
+    print(dic)
