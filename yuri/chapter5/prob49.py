17f64792b7b5cc518003f6c80adb1d56b97d7c31
村山友理 <Yuri@murayamayuri-no-MacBook-Air.local>
2019-06-19 15:13:48 +0900

    modify 49

--- yuri/chapter5/prob49.py
+++ yuri/chapter5/prob49.py
@@ -15,7 +15,18 @@ def mask(clause, sentence, charac):
                         flag = 1
                 else:
                     masked_clause += morph.surface.strip('。、')
-    return masked_clause
+    return masked_clause   
+
+def find_k(lst1, lst2):
+    for i, l in enumerate(lst1):
+        if l in lst2:
+            idx1 = i
+            break
+    for i, l in enumerate(lst2):
+        if l in lst1:
+            idx2 = i
+            break
+    return idx1, idx2
 
 def noun2noun(lst, sentence):
     line_lst = []
@@ -29,17 +40,18 @@ def noun2noun(lst, sentence):
                 idx = l.index(clause_j)
                 line = ' -> '.join([masked_clause_i] + l[1:idx] + [masked_clause_j]) 
             elif (set(l) & set(lst[i+x+1])) != None: # 上記以外で，文節iと文節jから構文木の根に至る経路上で共通の文節kで交わる場合
-                clause_k = list(set(l) & set(lst[i+x+1]))
-                idx1 = l.index(clause_k[0])
-                idx2 = lst[i+x+1].index(clause_k[0])
-                line = ' -> '.join([masked_clause_i] + l[1:idx1]) + '|' + ' -> '.join([masked_clause_j] + lst[i+x+1][1:idx2]) + '|' + ' -> '.join(clause_k)
+                idx1, idx2 = find_k(l, lst[i+x+1])
+                line = ' -> '.join([masked_clause_i] + l[1:idx1]) + ' |' + ' -> '.join([masked_clause_j] + lst[i+x+1][1:idx2]) + ' | ' + ' -> '.join(l[idx1:])
             else:
                 pass
-            line_lst.append(line)              
+            line_lst.append(line)             
     return line_lst
 
 if __name__ == '__main__':
     text = load_neko()
-    lst = noun2root(text[7])
-    for l in noun2noun(lst, text[7]):
+    lst7 = noun2root(text[7])
+    for l in noun2noun(lst7, text[7]):
+        print(l)
+    lst11 = noun2root(text[11])
+    for l in noun2noun(lst11, text[11]):
         print(l)
