1e795c325e4285370cc84e8408effe3b8f1009c5
Kaei <eifimeimeidola@gmail.com>
2019-07-02 09:58:49 +0900

    delete punct

--- kaei/chap06/nlp57.py
+++ kaei/chap06/nlp57.py
@@ -6,25 +6,36 @@ path = 'nlp.txt.xml'
 tree = ET.parse(path)
 root = tree.getroot()
 
-# sentencesとedges
+# 全文のリスト
 sen_lst = []
 edge_lst = []
+del_lst = [] # あとでノードから外すもの
 
 for sentence in root.iter('sentences'):
     for sen in sentence.iter('sentence'):
+        # 一文ごとのリスト
         words_lst = []
         dep_lst = []
+        del_part_lst = []
 
         for tok in sen.iter('token'):
             words_lst.append(tok[0].text)
         sen_lst.append(words_lst)
 
+        for d in range(len(words_lst)):
+            del_part_lst.append(d+1) # [1, 2, ...] edgeのindexと一緒
+
         for dep in sen.iter('dependencies'):
             if dep.attrib['type']=='collapsed-dependencies':
                 for de in dep.iter('dep'):
-                    dep_lst.append([int(de[0].attrib['idx']), int(de[1].attrib['idx'])])
+                    if de.attrib['type']!='punct':
+                        dep_lst.append([int(de[0].attrib['idx']), int(de[1].attrib['idx'])])
+                        if int(de[0].attrib['idx']) in del_part_lst:
+                            del_part_lst.remove(int(de[0].attrib['idx']))
+                        if int(de[1].attrib['idx']) in del_part_lst:
+                            del_part_lst.remove(int(de[1].attrib['idx']))
         edge_lst.append(dep_lst)
-
+        del_lst.append(del_part_lst)
 
 # グラフ
 for i, words in enumerate(sen_lst):
@@ -33,7 +44,8 @@ for i, words in enumerate(sen_lst):
 
     G.node('0', "ROOT")
     for j in range(len(words)): # ["Natural", "language", "processing", ...]
-        G.node(str(j+1), words[j]) # 単語を一つずつインデックス付きで
+        if j+1 not in del_lst[i]:
+            G.node(str(j+1), words[j]) # 単語を一つずつインデックス付きで
     
     for d in edge_lst[i]: # [[0, 18], [3, 1], ...]
         G.edge(str(d[0]), str(d[1]))
