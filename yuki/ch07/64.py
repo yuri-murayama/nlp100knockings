e2e04dc5fb5a0d0920c3e03cd91012f352a548b2
田屋侑希 <yuki@tayayuuki-no-MacBook-Air.local>
2019-07-17 15:08:02 +0900

    64.65

--- yuki/ch07/64.py
+++ yuki/ch07/64.py
@@ -5,12 +5,13 @@ client = MongoClient()
 db = client.testdb
 collection = db.artist
 
+
 with open('artist.json','r') as f:
     for line in f:
         line = json.loads(line)
         collection.insert_one(line)
 
-print(collection)
+# print(collection)
 collection.create_index('name')
 collection.create_index('aliased.name')
 collection.create_index('tags.value')
--- yuki/ch07/65.py
+++ yuki/ch07/65.py
@@ -4,5 +4,6 @@ client = MongoClient()
 db = client.testdb
 collection = db.artist
 
+
 for info in collection.find({'name':'Queen'}):
     print(info)
